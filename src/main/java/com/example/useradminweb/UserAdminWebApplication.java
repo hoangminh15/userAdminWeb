29ac25e43a0632128c8891bae889a44e1b0ac61f
hoangminh15 <hoangvanminhhust@gmail.com>
2021-02-23 08:38:20 +0700

    first commit

---
+++ src/main/java/com/example/useradminweb/UserAdminWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.useradminweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAdminWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserAdminWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/useradminweb/UserAdminWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.useradminweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAdminWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
